90a89164cc602c87dcfaecbfb2018d8d0eab1538
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2021-04-14 21:32:34 +0530

    added shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void Update()
+    {
+        Invoke("delMeth", 5);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect=Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+    public void delMeth()
+    {
+        Debug.Log("Hi");
+        Destroy(gameObject);
+    }
+}
