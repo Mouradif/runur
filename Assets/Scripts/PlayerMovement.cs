921a885db9ef530005db53fae2132fc77e69ba4d
Mourad Kejji <mkejji@gmail.com>
2021-04-15 15:42:45 +0200

    using knight no hands sprite + animation work in progress

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,28 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D _rb;
     private Vector2 _movement;
+
+    private Animator _animator;
+
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+
     // Update is called once per frame
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        var isRunning = Input.GetKey(KeyCode.LeftShift);
+        var speedMultiplier = (isRunning) ? 1.5f : 1f;
         _movement.x= Input.GetAxisRaw("Horizontal");
         _movement.y= Input.GetAxisRaw("Vertical");
-        _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+        _animator.SetBool(IsWalking, (_movement.x != 0 || _movement.y != 0));
+        _animator.SetBool(IsRunning, isRunning);
+
+        _rb.MovePosition(_rb.position + _movement * (moveSpeed * speedMultiplier * Time.fixedDeltaTime));
     }
 }
